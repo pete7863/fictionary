3883f9269bdaaa0f4328e6f9136ad9775f21baac
Nicholas Peterson <pete7863@Chelseas-MacBook-Pro.local>
2015-04-11 22:13:45 -0500

    Adding items to main Game module

--- Fictionary/Game.swift
+++ Fictionary/Game.swift
@@ -12,14 +12,28 @@ let MIN_NUM_PLAYERS = 2;
 let MAX_NUM_PLAYERS = 8;
 
 var Players:[Player] = []
+var CurrentPlayerID = 0
+var Round: Int = 1
 
-struct Player {
+public struct Player {
+    var ID: Int
     var Name: String = ""
-    var Fictalabras: [String] = []
+    var Sniglets: [Sniglet] = []
+    var CorrectGuesses: Int = 0
+    var GuessesReceived: Int = 0
     
     init(Name: String) {
+        self.ID = Players.count
         self.Name = Name
-        self.Fictalabras = []
+    }
+}
+
+public struct Sniglet {
+    var Word: String = ""
+    var GuessesReceived: Int = 0
+    
+    init(Word: String) {
+        self.Word = Word
     }
 }
 
@@ -27,6 +41,6 @@ public func AddNewPlayer(name: String) {
     Players.append(Player(Name: name))
 }
 
-public func AddFictalabra(index: Int, fictalabra: String) {
-    Players[index].Fictalabras.append(fictalabra)
+public func AddSniglet(index: Int, sniglet: Sniglet) {
+    Players[index].Sniglets.append(sniglet)
 }
